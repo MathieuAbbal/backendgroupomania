307b7d95fd3198ecee1952fd262eb739d7dbdc0b
Mathieu Abbal <mathieuabbal@hotmail.com>
2021-08-04 15:27:51 +0200

    test

--- models/index.js
+++ models/index.js
@@ -9,11 +9,11 @@ const loadModel = async ()=>{
   await Comment.belongsTo(User,{ foreignKey:'user_id', onDelete:'CASCADE'});
   await Like.belongsTo(Post,{ foreignKey:'post_id', onDelete:'CASCADE'});
   await Like.belongsTo(User,{ foreignKey:'user_id', onDelete:'CASCADE'}); 
-  await  database.query('set FOREIGN_KEY_CHECKS=0');
-  await User.sync({alter:true});
-  await Post.sync({alter:true});
-  await Comment.sync({alter:true});
-  await Like.sync({alter:true});
-  await  database.query('set FOREIGN_KEY_CHECKS=1');
+ // await  database.query('set FOREIGN_KEY_CHECKS=0');
+ // await User.sync({alter:true});
+ // await Post.sync({alter:true});
+ // await Comment.sync({alter:true});
+ // await Like.sync({alter:true});
+ // await  database.query('set FOREIGN_KEY_CHECKS=1');
 };
 module.exports = {User,Post,Comment,Like, loadModel};
