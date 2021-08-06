eb9c357f24bdc3695b6ce29842c3638047d5c5db
mathieu abbal <mathieuabbal@hotmail.com>
2021-08-06 14:41:30 +0200

    avatar url database

--- database_development.sql
+++ database_development.sql
@@ -91,7 +91,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `lastname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `avatar` varchar(255) DEFAULT 'http://localhost:3000/images/profil_default.jpg',
+  `avatar` varchar(255) DEFAULT 'http://backendgroupomania.herokuapp.com/images/profil_default.jpg',
   `bio` varchar(255) DEFAULT NULL,
   `isAdmin` tinyint(1) NOT NULL DEFAULT '0',
   `createdAt` datetime NOT NULL,
@@ -106,7 +106,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `avatar`, `bio`, `isAdmin`, `createdAt`, `updatedAt`) VALUES
-(5, 'Admin', 'Admin', 'admin@mail.com', '$2b$10$M1LlIkwJgDXMAEpZ2vsKj.CDCYNVM2xv5aiLCM3IvZ/PcLe6CcZPG', 'http://localhost:3000/images/Admin.jpg', 'Je suis l\'administrateur du réseau social !', 1, '2021-07-22 13:21:46', '2021-07-26 14:30:42');
+(5, 'Admin', 'Admin', 'admin@mail.com', '$2b$10$M1LlIkwJgDXMAEpZ2vsKj.CDCYNVM2xv5aiLCM3IvZ/PcLe6CcZPG', 'http://backendgroupomania.herokuapp.com/images/Admin.jpg', 'Je suis l\'administrateur du réseau social !', 1, '2021-07-22 13:21:46', '2021-07-26 14:30:42');
 
 --
 -- Contraintes pour les tables déchargées
